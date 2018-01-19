bcd47899f200cd8e975b17d35a0ef204f048a3a7
Chris Chapman <chris@pentandra.com>
2018-01-18 20:36:34 -0700

    Simplify create-agenda command

--- commands/create-agenda.rb
+++ commands/create-agenda.rb
@@ -1,31 +1,26 @@
 # frozen_string_literal: true
 
-usage 'create-agenda [options] date & time (in UTC)'
+usage 'create-agenda [options] date & time'
 aliases :create_agenda, :ca
 summary 'create a meeting agenda'
-description 'Create a new meeting agenda for the given UTC date and time.'
+description 'Create a new meeting agenda for the given date and time.'
 flag nil, :force, 'force creation of new agenda'
 
 class CreateAgenda < ::Nanoc::CLI::CommandRunner
 
   def run
     require 'chronic'
-    require 'active_support/core_ext/time/zones'
-    require 'active_support/core_ext/time/conversions'
 
     # Extract arguments
     if arguments.length != 1
       raise Nanoc::Int::Errors::GenericTrivial, "usage: #{command.usage}"
     end
 
-    Time.zone = 'UTC'
-    Chronic.time_class = Time.zone
     meeting_time = Chronic.parse(arguments[0])
-    #config = Nanoc::Int::ConfigLoader.new.new_from_cwd
     path = "items/meetings/#{meeting_time.strftime('%F')}/"
 
     # Check whether agenda exists
-    if File.exist?(path) && (!File.directory?(path) || !(Dir.entries(path) - %w[. ..]).empty?) && !options[:force]
+    if File.exist?(path) && !options[:force]
       raise(
         Nanoc::Int::Errors::GenericTrivial,
         "The agenda was not created because '#{path}' already exists. " \
@@ -38,35 +33,26 @@ class CreateAgenda < ::Nanoc::CLI::CommandRunner
       Nanoc::CLI::Logger.instance.file(:high, :create, file_path)
     end
 
-    # Build entire site
+    # Build agenda
     FileUtils.mkdir_p(path)
-    FileUtils.cd(File.join(path)) do
-      write('agenda.md', <<~EOS
+
+    agenda = <<~EOS
     ---
     title: #{meeting_time.strftime('%F')} SCWG WG4 Weekly Meeting
-    meeting_time: #{meeting_time}
-    message:
+    meeting_time: #{meeting_time.utc.iso8601}
     ---
+    # Proposed Agenda
 
-    1 Agenda review
-    2 Introductions to new people
-    3 Status of current action items
-      - Start a narrative of our progress https://goo.gl/xs496P [CHRIS - DONE]
-      - Add to the scenarios. https://goo.gl/XikbTf [ONGOING]
-      - Add to the questions to consider: https://goo.gl/MxA41E [ONGOING]
-    4 Our goal and how we can productively work towards it
-      - Discussion on how to track proposed topics and our progress/analysis
-    5 Various discussion topics (as time permits)
-      - Scenarios: https://goo.gl/XikbTf
-      - Questions to consider: https://goo.gl/MxA41E
-      - Technologies: https://goo.gl/8r822b
-    6 Action items and suggestions for next week's agenda
+    1. Agenda review
+    2. Introductions to new people
+    3. Status of current action items
+    4. Our goal and how we can productively work towards it
+    5. Various discussion topics (as time permits)
+    6. Action items and suggestions for next week's agenda
+    7. Post call summary and wrap-up
 EOS
-           )
-    end
 
-    puts "Created an agenda at '#{path}' for " \
-         "#{meeting_time.to_time.to_s(:long_ordinal)} (#{meeting_time.strftime('%H:%M %Z')}). Enjoy!"
+    write(File.join(path, 'agenda.md'), agenda)
   end
 
   private
