120a13aeb8f97c873a6758f74a5cbf62d4c70084
Chris Chapman <chris@pentandra.com>
2018-05-31 10:13:45 -0600

    Properly parse the WebVTT into human-readable, plain text
    - Change the filter to output markdown instead of plain text
    - Use Pandoc to handle the wrapping and indention
    - Refactor output according to ux testing
    - Update the transcript mail layout including linking to meeting archive

---
+++ lib/filters/vtt2md.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal = true
+
+class WebVTT2Markdown < Nanoc::Filter
+  identifier :vtt2md
+
+  requires 'nokogiri', 'webvtt', 'active_support/core_ext/string/indent'
+
+  def run(content, params = {})
+    webvtt = WebVTT.from_blob(content)
+
+    webvtt.cues
+      .flat_map { |cue| cue.text.scan(/<v (.+?)>(.+?)(?:<\/v>|\z)/) }
+      .chunk { |cue| cue.first }
+      .map do |voice, cues|
+        statement = ::Nokogiri::HTML.fragment(cues.map(&:last).join(' ')).text
+        "#{voice}\n:#{statement.indent(3)}\n\n"
+      end.join
+  end
+end
